3d3c124876733b53048e77e3fe9a7d3ac1f9bd40
xu.wang <wangxu_cn@icloud.com>
2024-03-31 19:40:35 +0800

    Update bg.php

--- api/bg.php
+++ api/bg.php
@@ -1,16 +1,17 @@
 <?php
     $str=file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
-if (preg_match("/<urlBase>(.+?)<\/urlBase>/ies", $str, $matches)) {
-    $imgurl='https://cn.bing.com'.$matches[1].'_1920x1080.jpg';
-}
-if ($imgurl) {
-    header('Content-Type: image/JPEG');
-    @ob_end_clean();
-    @readfile($imgurl);
-    @flush();
-    @ob_flush();
-    exit();
-} else {
-    exit('error');
-}
+echo $str;
+//if (preg_match("/<urlBase>(.+?)<\/urlBase>/ies", $str, $matches)) {
+//    $imgurl='https://cn.bing.com'.$matches[1].'_1920x1080.jpg';
+//}
+//if ($imgurl) {
+//    header('Content-Type: image/JPEG');
+//    @ob_end_clean();
+//    @readfile($imgurl);
+//    @flush();
+//    @ob_flush();
+//    exit();
+//} else {
+//    exit('error');
+//}
 ?>
