aca12c62dd0132ae7f4a9bc3725ec914c930dc73
Asplla <wangxu_cn@icloud.com>
2024-09-10 00:09:45 +0800

    uplpad

--- api/module/releases.php
+++ api/module/releases.php
@@ -8,8 +8,8 @@ if(!defined('IN_API')) {
 	exit('Access Denied');
 }
 
-$user = $_GET['user'];
-$repo = $_GET['repo'];
+$user = trim($_GET['user'] ?? '');
+$repo = trim($_GET['repo'] ?? '');
 
 if(!$user) {
   return_json('401', 'Please Enter Github Username');
@@ -18,5 +18,22 @@ if(!$user) {
 if(!$repo) {
   return_json('401', 'Please Enter Github Repository');
 }
+
+$url = "https://api.github.com/repos/".$user."/".$repo."/releases/latest";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  return_json('401', $err);
+}
+
+$data = json_decode($response, true);
+return_json('200', 'success', $data);
 
 ?>
