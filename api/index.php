0bddb5cb786e1afcc4533562a6bcd1c0d28e636c
Asplla <wangxu_cn@icloud.com>
2024-09-09 23:59:53 +0800

    uplpad

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@
 define('IN_API', true);
 require_once "function/function_common.php";
 
-$mod = $_GET['mod'];
+$mod = trim($_GET['mod'] ? $_GET['mod'] : '');
 $mod = in_array($mod, array('user')) ? $mod : "index";
 
 require_once "module/".$mod.".php";
