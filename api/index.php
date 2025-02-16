ac23e626bd20074292656a0382d409654af98136
iván <76005434@qq.com>
2025-02-16 10:38:41 +0800

    update sendemail

--- api/index.php
+++ api/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *	Powered by wxss.fit
  *  Email:minbbs@qq.com
@@ -8,8 +9,6 @@ define('IN_API', true);
 require_once "function/function_common.php";
 
 $mod = trim($_GET['mod'] ?? '');
-$mod = in_array($mod, array('releases')) ? $mod : "index";
+$mod = in_array($mod, array('releases', 'alipan', 'sendemail')) ? $mod : "index";
 
-require_once "module/".$mod.".php";
-
-?>
+require_once "module/" . $mod . ".php";
