8bd7f9f5a266ea6615544fca12fb5716b9281f3c
iván <76005434@qq.com>
2025-02-17 19:33:41 +0800

    update

--- api/module/sendemail.php
+++ api/module/sendemail.php
@@ -72,8 +72,7 @@ $result = $smtp->send(
 
 // 返回结果
 if ($result) {
-    $debug_info = $smtp->getDebugInfo(); // 获取调试信息
-    return_json('200', '留言已成功发送', array('debug' => $debug_info));
+    return_json('200', '留言已成功发送');
 } else {
     return_json('500', '发送失败：' . $smtp->getError());
 }
